6f0a5472bc592134772d72f1031f45df857045d5
jacobmorency <jvmorency@gmail.com>
2025-07-08 23:14:45 -0400

    Update projectcard.tsx

--- jacob-portfolio/src/components/projects/projectcard.tsx
+++ jacob-portfolio/src/components/projects/projectcard.tsx
@@ -1,14 +1,51 @@
-export default function ProjectCard() {
+import Image from "next/image";
+import { Github, Globe } from "lucide-react";
+
+type ProjectCardProps = {
+  title: string;
+  description: string;
+  link: string;
+  src: string;
+};
+
+export default function ProjectCard({
+  title,
+  description,
+  link,
+  src,
+}: ProjectCardProps) {
   return (
-    <div className="border-3 border-primary rounded-md p-4 flex flex-col items-center justify-center gap-2">
-      <h3>Project Title</h3>
-      <p>
-        Project description goes here. It can be a brief overview of the
-        project.
-      </p>
-      <a href="#" className="text-blue-500 hover:underline">
-        View Project
-      </a>
+    <div className="p-4 flex flex-col items-center bg-card max-w-sm rounded-lg shadow-md min-h-150 max-h-150">
+      <div
+        className="rounded-md bg h-48 w-full hover:scale-105 transition-transform duration-300 ease-in-out"
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgb(0, 0, 0, 0.4) , var(--card)), url(${src})`,
+          backgroundSize: "cover",
+        }}
+      ></div>
+      <div className="flex flex-col justify-between flex-1 w-full">
+        <div className="flex flex-col w-full flex-1">
+          <h3 className="text-3xl mb-2">{title}</h3>
+          <p className="text-lg text-muted-foreground">
+            {description ||
+              "Project description goes here. It can be a brief overview of the project."}
+          </p>
+        </div>
+        <div className="flex justify-center gap-2 mt-4">
+          <a
+            href="#"
+            className="text-primary border border-primary p-2 rounded-md hover:bg-primary hover:text-background transition-colors duration-300"
+          >
+            <Globe />
+          </a>
+          <a
+            href="#"
+            className="text-primary border border-primary p-2 rounded-md hover:bg-primary hover:text-background transition-colors duration-300"
+          >
+            <Github />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- jacob-portfolio/src/components/projects/projectssection.tsx
+++ jacob-portfolio/src/components/projects/projectssection.tsx
@@ -1,6 +1,10 @@
 import ProjectCard from "@/components/projects/projectcard";
 
 export default function ProjectsSection() {
+  const riveDescription =
+    "Rive is a lifting tracker app that allows users to log their workouts, track their progress, and visualize their lifting data. It features a user-friendly interface, real-time data updates, and integration with various fitness APIs to provide a comprehensive lifting experience.";
+  const bingeListDescription =
+    "BingeList is a movie and TV show tracking app that helps users keep track of what they have watched, want to watch, and are currently watching. It includes features like favorites, watchlists, and search functionality.";
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold">Projects</h1>
@@ -9,19 +13,18 @@ export default function ProjectsSection() {
         my skills in full stack web development, utilizing various technologies
         and frameworks.
       </p>
-      <div>
-        <div className="grid grid-cols-2">
-          <ProjectCard />
-          <div></div>
-        </div>
-        <div className="grid grid-cols-2">
-          <div></div>
-          <ProjectCard />
-        </div>
-        <div className="grid grid-cols-2">
-          <ProjectCard />
-          <div></div>
-        </div>
+      <div className="grid grid-cols-3 gap-4 my-4">
+        <ProjectCard
+          title="Rive - Lifting Tracker"
+          src="/images/rive.png"
+          description={riveDescription}
+        />
+        <ProjectCard
+          title="BingeList"
+          src="/images/rive.png"
+          description={bingeListDescription}
+        />
+        <ProjectCard title="TBD" src="/images/rive.png" />
       </div>
     </div>
   );
