212b48cac6e99cf67b6135716503e5c4decf000c
jacobmorency <jvmorency@gmail.com>
2025-07-23 23:14:34 -0400

    Update navbar styles

--- jacob-portfolio/src/components/ui/navbar.tsx
+++ jacob-portfolio/src/components/ui/navbar.tsx
@@ -3,16 +3,24 @@ export default function NavBar() {
     <nav className="sticky top-0 shadow-md p-4 bg-primary my-4">
       <ul className="flex space-x-8 justify-end">
         <li>
-          <a href="#home">Home</a>
+          <a className="text-lg font-bold" href="#home">
+            Home
+          </a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <a className="text-lg font-bold" href="#about">
+            About
+          </a>
         </li>
         <li>
-          <a href="#projects">Projects</a>
+          <a className="text-lg font-bold" href="#projects">
+            Projects
+          </a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a className="text-lg font-bold" href="#contact">
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
