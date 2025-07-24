c0c29bf61342463c0456bf168e3963755332b7de
jacobmorency <jvmorency@gmail.com>
2025-07-23 23:14:09 -0400

    Update removed dead code

--- jacob-portfolio/src/components/about/skill.tsx
+++ jacob-portfolio/src/components/about/skill.tsx
@@ -2,7 +2,7 @@ type SkillProps = { title: string };
 
 export default function Skill({ title = "Skill Name" }: SkillProps) {
   return (
-    <div className=" bg-primary rounded-md p-4 flex flex-col items-center justify-center gap- w-30 h-30">
+    <div className=" bg-primary rounded-md flex flex-col items-center justify-center">
       <h3 className="text-center">{title}</h3>
     </div>
   );
