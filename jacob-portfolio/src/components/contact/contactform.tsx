ad9657563422fc551c5bb46b268c6e848a8b05a5
jacobmorency <jvmorency@gmail.com>
2025-07-24 10:15:25 -0400

    Update contact form styles

--- jacob-portfolio/src/components/contact/contactform.tsx
+++ jacob-portfolio/src/components/contact/contactform.tsx
@@ -5,19 +5,47 @@ import { Textarea } from "@/components/ui/textarea";
 
 export default function ContactForm() {
   return (
-    <form className="flex flex-col gap-4">
-      <Label htmlFor="name">Your Name</Label>
-      <Input id="name" type="text" placeholder="Your Name" required />
-      <Label htmlFor="email">Your Email</Label>
-      <Input id="email" type="email" placeholder="Your Email" required />
-      <Label htmlFor="message">Your Message</Label>
-      <Textarea
-        id="message"
-        placeholder="Your Message"
-        className="h-32"
-        required
-      />
-      <Button type="submit">Send Message</Button>
-    </form>
+    <div>
+      <form className="flex flex-col gap-4">
+        <div>
+          <Label htmlFor="name" className="font-semibold my-2">
+            Your Name
+          </Label>
+          <Input
+            id="name"
+            type="text"
+            placeholder="Your Name"
+            className="rounded py-6"
+            required
+          />
+        </div>
+        <div>
+          <Label htmlFor="email" className="font-semibold my-2">
+            Your Email
+          </Label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="Your Email"
+            className="rounded py-6"
+            required
+          />
+        </div>
+        <div>
+          <Label htmlFor="message" className="font-semibold my-2">
+            Your Message
+          </Label>
+          <Textarea
+            id="message"
+            placeholder="Your Message"
+            className="h-32 rounded"
+            required
+          />
+        </div>
+        <Button type="submit" className="py-6 rounded">
+          Send Message
+        </Button>
+      </form>
+    </div>
   );
 }
